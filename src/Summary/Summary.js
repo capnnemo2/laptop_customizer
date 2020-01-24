684e0791633466cf79814a898d361ce366626c4e
Ben Hernandez <benjammin234@gmail.com>
2020-01-24 08:05:10 -0800

    Adds Cart component

--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -1,30 +1,16 @@
 import React from 'react';
 import './Summary.css';
-import USCurrencyFormat from '../USCurrencyFormat/USCurrencyFormat';
 import Total from '../Total/Total';
+import Cart from '../Cart/Cart';
 
 
 export default class Summary extends React.Component {
     render() {
-        const summary = Object.keys(this.props.selected).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const selectedOption = this.props.selected[feature];
-            return (
-
-                <div className="summary__option" key={featureHash}>
-                    <div className="summary__option__label">{feature} </div>
-                    <div className="summary__option__value">{selectedOption.name}</div>
-                    <div className="summary__option__cost">
-                        {USCurrencyFormat.format(selectedOption.cost)}
-                    </div>
-                </div>
-            );
-        });
 
         return (
             <section className="main__summary">
                 <h2>Your cart</h2>
-                {summary}
+                <Cart {...this.props} />
 
                 <Total {...this.props} />
 
